4835fc8d46e8ce03779dbf3c80c541534a9f9b6e
Jeremy Keeton <jkeeton@tableau.com>
2018-01-01 22:18:29 -0800

    drills and projects

--- bokehPractice.py
+++ bokehPractice.py
@@ -18,43 +18,52 @@
 
 # this one works
 
+# from bokeh.plotting import figure, output_file, show
+#
+# # prepare some data
+# x = [1, 2, 3, 4, 5]
+# y = [6, 7, 2, 4, 5]
+#
+# # output to static HTML file
+# output_file("lines.html", title="line plot example")
+#
+# # create a new plot with a title and axis labels
+# p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
+#
+# # add a line renderer with legend and line thickness
+# p.line(x, y, legend="Temp.", line_width=2)
+#
+# # show the results
+# show(p)
+
+
+
+# this is my attempt to redo the Meat Consumption Viz for MakeoverMonday
+# http://bokeh.pydata.org/en/latest/docs/user_guide/quickstart.html#userguide-quickstart
+
 from bokeh.plotting import figure, output_file, show
-
-# prepare some data
-x = [1, 2, 3, 4, 5]
-y = [6, 7, 2, 4, 5]
-
-# output to static HTML file
-output_file("lines.html", title="line plot example")
-
-# create a new plot with a title and axis labels
-p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
-
-# add a line renderer with legend and line thickness
-p.line(x, y, legend="Temp.", line_width=2)
-
-# show the results
+output_file("test.html")
+p = figure()
+p.line([1, 2, 3, 4, 5], [6, 7, 2, 4, 5], line_width=2)
 show(p)
-
-
 
 # <><><><><><<>
 
-# this is matplolib
-import matplotlib.pyplot as plt;
-
-plt.rcdefaults()
-import numpy as np
-import matplotlib.pyplot as plt
-
-objects = ('Stanford', 'Washington', 'Penn State', 'Nebraska', 'Hawaii', 'UCLA')
-y_pos = np.arange(len(objects))
-performance = [1, 8, 15, 12, 7, 9]
-
-plt.bar(y_pos, performance, align='center', alpha=0.9)
-plt.xticks(y_pos, objects)
-plt.ylabel('Wins')
-plt.title('NCAA Women\'s Volleyball Champs')
-
-plt.show()
-# plt.
+# # this is matplolib
+# import matplotlib.pyplot as plt;
+#
+# plt.rcdefaults()
+# import numpy as np
+# import matplotlib.pyplot as plt
+#
+# objects = ('Stanford', 'Washington', 'Penn State', 'Nebraska', 'Hawaii', 'UCLA')
+# y_pos = np.arange(len(objects))
+# performance = [1, 8, 15, 12, 7, 9]
+#
+# plt.bar(y_pos, performance, align='center', alpha=0.9)
+# plt.xticks(y_pos, objects)
+# plt.ylabel('Wins')
+# plt.title('NCAA Women\'s Volleyball Champs')
+#
+# plt.show()
+# # plt.
---
+++ mapPlotLib2.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+# x = [2, 4, 6]
+# y = [1, 3, 5]
+# plt.plot(x, y)
+# plt.show()
+
+Year = [1970, 1980, 1990, 2000, 2010]
+Pork = [55.4, 56.8, 49.4, 50.8, 47.2]
+Beef = [84.4, 76.4, 67.5, 67.5, 59.4]
+Chicken = [36.6, 45.8, 59.5, 77, 82.4]
+Fish = [11.8, 12.5, 15, 15.2, 15.8]
+
+
+plt.plot(Year, Pork, color='tan')
+plt.plot(Year, Beef, color='red')
+plt.plot(Year, Chicken, color='blue')
+plt.plot(Year, Fish, color='green')
+plt.xlabel('Decade')
+plt.ylabel('Pounds Eaten')
+plt.title('U.S. Per Capita Meat Consumption - 1970 - 2010')
+plt.show()
