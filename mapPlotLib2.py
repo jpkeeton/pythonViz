5f1adcf4d981b2693b8390eeab4674b53e0132d2
Jeremy Keeton <jkeeton@tableau.com>
2018-01-02 14:24:01 -0800

    removed some bits

--- mapPlotLib2.py
+++ mapPlotLib2.py
@@ -1,11 +1,6 @@
 import matplotlib.pyplot as plt
 # http://blog.adnansiddiqi.me/data-visualization-in-python-pie-charts-in-matplotlib/
 
-# x = [2, 4, 6]
-# y = [1, 3, 5]
-# plt.plot(x, y)
-# plt.show()
-
 Year = [1970, 1980, 1990, 2000, 2010]
 Pork = [55.4, 56.8, 49.4, 50.8, 47.2]
 Beef = [84.4, 76.4, 67.5, 67.5, 59.4]
